a7d7a3f1cb44b78da895d8d51b25cd0eb458a0ac
Pedro Brito <pedrodebrito2202@gmail.com>
2024-07-29 00:41:17 -0300

    Códigos comentados.

--- ProjetoCRUD/Controllers/HomeController.cs
+++ ProjetoCRUD/Controllers/HomeController.cs
@@ -8,8 +8,10 @@ namespace ProjetoCRUD.Controllers
     {
         public IActionResult Index()
         {
+            // Aqui é onde instanciamos a model dentro da controller para que possamos aplicar o tratamento dos dados
             HomeModel home = new HomeModel();
 
+            // Dessa forma é como colocamos dados manualmente nos objetos.
             home.Nome = "Pedro";
             home.Email = "pedrodebrito2202@gmail.com";
 
--- ProjetoCRUD/Models/HomeModel.cs
+++ ProjetoCRUD/Models/HomeModel.cs
@@ -2,6 +2,7 @@
 {
     public class HomeModel
     {
+        // Aqui criamos as classes para que possamos colocar dados
         public string Nome { get; set; }
         public string Email { get; set; }
 
