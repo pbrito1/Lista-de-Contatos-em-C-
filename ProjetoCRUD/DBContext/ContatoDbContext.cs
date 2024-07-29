bc0b1764c517b2b46cf0198e81f058fa8d142e07
Pedro Brito <pedrodebrito2202@gmail.com>
2024-07-29 12:58:33 -0300

    Continuação da conexão com a databse

---
+++ ProjetoCRUD/DBContext/ContatoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoCRUD.DBContext
+{
+    public class ContatoDbContext : DbContext
+    {
+        public ContatoDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+        public DbSet<Contatos> Contatos { get; set; }
+    }
+}
---
+++ ProjetoCRUD/DBContext/Contatos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoCRUD.DBContext
+{
+    public class Contatos
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("nome", TypeName ="Varchar(200)")]
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public double Telefone { get; set; }
+    }
+}
