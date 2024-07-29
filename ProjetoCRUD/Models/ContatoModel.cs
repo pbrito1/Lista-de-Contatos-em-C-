cd010eec5c805205affea6c09fab12c965b3d105
Pedro Brito <pedrodebrito2202@gmail.com>
2024-07-29 12:37:03 -0300

    Subindo a conexão com o banco de dados.

--- ProjetoCRUD/Models/ContatoModel.cs
+++ ProjetoCRUD/Models/ContatoModel.cs
@@ -2,6 +2,7 @@
 {
     public class ContatoModel
     {
+        public int Id { get; set; } 
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public int Celular { get; set; }
